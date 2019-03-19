bfc0ced5e2c12a837ebd995b4b3b8bc5de0990b6
Jay Zheng <jay.j.zheng@gmail.com>
2019-03-19 13:54:11 -0700

    add mock client

---
+++ client/mock.go
@@ -0,0 +1,27 @@
+package client
+
+import (
+	"io"
+	"net/http"
+)
+
+type Mock struct {
+	Fn      func(*http.Request) (*http.Response, error)
+	Invoked bool
+}
+
+func (m *Mock) Do(req *http.Request) (*http.Response, error) {
+	m.Invoked = true
+	return m.Fn(req)
+}
+
+func NewMock(status int, rc io.ReadCloser, err error) *Mock {
+	return &Mock{
+		Fn: func(_ *http.Request) (*http.Response, error) {
+			return &http.Response{
+				Body:       rc,
+				StatusCode: http.StatusOK,
+			}, err
+		},
+	}
+}
