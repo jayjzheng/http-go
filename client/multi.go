d9f58a437d35b05f97e76c5e41defd1211be3604
Jay Zheng <jay.j.zheng@gmail.com>
2019-04-04 22:30:25 -0700

    refactor to use fan out

--- client/multi.go
+++ client/multi.go
@@ -1,8 +1,12 @@
 package client
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"sync"
+
+	"github.com/hashicorp/go-multierror"
 )
 
 type Multi struct {
@@ -20,32 +24,109 @@ type Response struct {
 }
 
 // Get attempts to fetch from urls, and returns a <-chan of Response.
-func (m *Multi) Get(urls []string, vv ...validator) <-chan Response {
-	limit := m.ConcurrencyLimit
-	if limit == 0 {
-		limit = len(urls)
+func (m *Multi) Get(ctx context.Context, urls []string, vv ...validator) <-chan Response {
+	in := m.generate(urls)
+	limit := m.limit(len(urls))
+
+	cs := make([]<-chan Response, limit)
+	for i := 0; i < limit; i++ {
+		cs[i] = m.get(ctx, in, vv)
 	}
 
-	ch := make(chan Response, limit)
+	return m.merge(ctx, cs...)
+}
 
-	for _, u := range urls {
-		go func(u string) {
+// Handle drains the resp channel and invokes handler fn for reach Response.
+func (m *Multi) Handle(resp <-chan Response, fn func(Response) error) error {
+	var errs *multierror.Error
+
+	for r := range resp {
+		if r.Err != nil {
+			errs = multierror.Append(errs, r.Err)
+			continue
+		}
+
+		if err := fn(r); err != nil {
+			errs = multierror.Append(errs, r.Err)
+			continue
+		}
+	}
+
+	return errs.ErrorOrNil()
+}
+
+func (m *Multi) generate(urls []string) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		defer close(out)
+		for _, u := range urls {
+			out <- u
+		}
+	}()
+
+	return out
+}
+
+func (m *Multi) get(ctx context.Context, urls <-chan string, vv []validator) <-chan Response {
+	out := make(chan Response)
+	go func() {
+		defer close(out)
+		for u := range urls {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
 			resp, err := m.Client.Get(u)
 			if err != nil {
-				ch <- Response{Err: err}
-				return
+				out <- Response{Err: err}
+				continue
 			}
 			for _, validate := range vv {
 				if err := validate(resp); err != nil {
-					ch <- Response{Err: err}
+					out <- Response{Err: err}
+					continue
+				}
+			}
+			out <- Response{Response: resp}
+		}
+	}()
+	return out
+}
+
+func (m *Multi) merge(ctx context.Context, cs ...<-chan Response) <-chan Response {
+	var wg sync.WaitGroup
+	wg.Add(len(cs))
+
+	out := make(chan Response)
+
+	for _, c := range cs {
+		go func(c <-chan Response) {
+			defer wg.Done()
+			for r := range c {
+				select {
+				case out <- r:
+				case <-ctx.Done():
 					return
 				}
 			}
-			ch <- Response{Response: resp}
-		}(u)
+		}(c)
 	}
 
-	return ch
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func (m *Multi) limit(count int) int {
+	if m.ConcurrencyLimit == 0 {
+		return count
+	}
+	return m.ConcurrencyLimit
 }
 
 func ValidateStatusOK(resp *http.Response) error {
